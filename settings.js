8251c3267f257f2081daae634d3d3de3bc5a5698
SosmithOsei <sosmithosei@gmail.com>
2025-05-26 05:17:07 +0000

    settings.js

--- settings.js
+++ settings.js
@@ -16,7 +16,7 @@ if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
 //--------------------[ SESSION ID ]----------------------//
 
 global.SESSION_ID = process.env.SESSION_ID || '' 
-//Enter your KANGO-XMD session id here; must start with KANGO~
+//KANGO~VyclibyQ#slpfHqOr4T1GJfMIdRQOPw4v2IjQUnOEcZsLcCRMOfo
 
 //--------------------[ BOT NAME ]----------------------//
 
